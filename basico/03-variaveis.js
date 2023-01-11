40fb9db02bbc6369fed2fc10e75da7d2108809f0
Cristiano Ledur <cristianoledur98@gmail.com>
2023-01-11 10:57:45 -0300

    Upload arquivo 'variaveis.js'

---
+++ basico/03-variaveis.js
@@ -0,0 +1,10 @@
+console.log("1.3 - Básico - Variáveis");
+
+// var -> Torna oescopo da variável global.
+console.log("variáveis var");
+
+// let -> podemos declarar variáveis com escopo local dentro de blocos de instrução (IF, FOR, WHILE...).
+console.log("variáveis let");
+
+// const -> valor fixo, ou seja, não é possível atribuir um novo valor.
+console.log("variáveis const");
